ddfadff649e28caddf31eb750aaeddf15e249c67
Kazufumi Okuda <kazu.okuda0205@gmail.com>
2019-03-24 19:35:07 +0900

    change file name

---
+++ mod_pow.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll mod_pow(int n, int p, int mod){
+    // return (n^p) % mod
+    if(p==0) return 1;
+    ll res = mod_pow(n*n%mod, n/2, mod);
+    if(p%2==1) res = res * n % mod;
+    return res;
+}
