ec2ed6062e99b97dffcc43f71344ca6d125bface
Kazufumi Okuda <kazu.okuda0205@gmail.com>
2019-03-27 19:44:06 +0900

    modify mod_pow.cpp

--- mod_pow.cpp
+++ mod_pow.cpp
@@ -5,7 +5,7 @@ typedef long long ll;
 ll mod_pow(int n, int p, int mod){
     // return (n^p) % mod
     if(p==0) return 1;
-    ll res = mod_pow(n*n%mod, n/2, mod);
+    ll res = mod_pow(n*n%mod, p/2, mod);
     if(p%2==1) res = res * n % mod;
     return res;
 }
