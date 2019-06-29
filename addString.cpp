afc1f92d07c3102e4e7ba957718594880d7d4775
ya-poo <kazu.okuda0205@gmail.com>
2019-06-29 16:11:32 +0900

    add addString

---
+++ addString.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string addString(string s, string t){
+    string ans = "";
+    if(s.size() > t.size()){
+        swap(s, t);
+    }
+    while(t.size()>s.size()){
+        s = '0' + s;
+    }
+    int dig = 0;
+    int n = s.size();
+    for(int i=0; i<n; i++){
+        int ret = dig + (s[n-i-1]-'0')+(t[n-i-1]-'0');
+        dig = ret/10;
+        ret %= 10;
+        ans=(char)(ret+'0') + ans;
+    }
+    if(dig)ans = '1' + ans;
+    return ans;
+}
